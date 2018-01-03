be0a911a0faf2fc45c71acd2767b6f2ed0b3f0db
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2018-01-03 15:55:24 +0100

    Fixed CRAIG loading error

--- memtest-sb-mutator/modinfo.lua
+++ memtest-sb-mutator/modinfo.lua
@@ -1,11 +1,11 @@
 return {
   name='SpringBoard MemTest',
   description='Scenario Editor for Spring: 1944',
-  shortname='SB MemTest',
+  shortname='SB-MemTest',
   version='$VERSION',
   mutator='Official',
   game='SpringBoard MemTest',
-  shortGame='SB MemTest',
+  shortGame='SB-MemTest',
   modtype=1,
   depend = {
     'Spring: 1944 $VERSION',
